793971d85268740bff9f333a34d9169537571f18
bright_spark <markpassey@btinternet.com>
2019-05-12 01:04:47 +0100

    Added an item

--- src/main/kotlin/brightspark/testmodz/Main.kt
+++ src/main/kotlin/brightspark/testmodz/Main.kt
@@ -5,6 +5,7 @@ import com.zerra.api.mod.ModInit
 import com.zerra.api.mod.info.ModInfo
 import com.zerra.api.mod.info.ModInfoBuilder
 import com.zerra.common.Reference
+import com.zerra.common.world.item.Item
 
 class Main : Mod {
     override val modInfo: ModInfo
@@ -12,6 +13,7 @@ class Main : Mod {
 
     override fun init(modInit: ModInit) {
         logger.info("Hey, listen!")
+        modInit.registerItem(Item("lightbulb"))
     }
 
     override fun postInit() {
